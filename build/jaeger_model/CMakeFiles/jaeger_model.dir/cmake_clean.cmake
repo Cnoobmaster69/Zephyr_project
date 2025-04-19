b6f27dd19fbee9480b9fb6350dd6df764e4ebd3a
CarlosCortes <carlos.eduardo.cortes.cecc@gmail.com>
2025-04-18 23:26:51 -0500

    Primer commit

---
+++ build/jaeger_model/CMakeFiles/jaeger_model.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jaeger_model.dir/src/hello_world.cc.o"
+  "CMakeFiles/jaeger_model.dir/src/hello_world.cc.o.d"
+  "libjaeger_model.pdb"
+  "libjaeger_model.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jaeger_model.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/jaeger_model/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/carlos/ros2_ws/src/jaeger_model
+# Build directory: /home/carlos/ros2_ws/src/jaeger_model/build/jaeger_model
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
